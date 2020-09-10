efdd034e269724318f35b8a8c799f2002eb4be3d
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2020-09-10 11:09:32 +0530

    Code

---
+++ Password Encryption.py
@@ -0,0 +1,11 @@
+import random
+if __name__ == "__main__":
+    inp = input("Enter password to be encrypted: \n")
+    SECURE = (('s', '$'),('i', '|'), ('x', '*'), ('k','+'), ('g','4'),('4','f'),('f','4'),('a','@'),('@','a'),('o','0'),('X','x'),('%','#'),('w','^'),('h','='),('H','~'),('r',','),('e','/'),('t','<'),('q','>'),('l','?'),('c','1'))
+    def securepassword(password):
+        for a,b in SECURE:
+            password = password.replace(a, b)
+        return password
+    password = inp
+    password = securepassword(password)
+    print(f"Your Encrpted Password is: {password}")
