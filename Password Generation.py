4221e7415731e70355d85068b68542e46f43aab1
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2020-09-10 11:56:01 +0530

    Code

--- Password Generation.py
+++ Password Generation.py
@@ -1,19 +1,19 @@
-import string
-import random
+import string                                      #built-in module
+import random                                      #built-in module
 if __name__ == "__main__":
-    a1 = string.printable
-    i = input("Enter length of password: \n")
+    a1 = string.printable                          #taking all characters,digits and punctuations as string
+    i = input("Enter length of password: \n")      #taking length of password required as input
     while True:
 	        
-	        if i.isdigit():
+	        if i.isdigit():                    #checking whether input is interger.
 		        break
 	        else:
 		        print("Please enter a number")
 		        continue
-    a = []
-    a.extend(list(a1))    
-    random.shuffle(a)
+    a = []                                         #taking empty list
+    a.extend(list(a1))                             #appending all elements in empty list.
+    random.shuffle(a)                              #shuffling all elements and choosing random elements
     print("Your Password is: " ,end="")
     c = "".join(a[0:int(i)])
-    print(c)
+    print(c)                                       #printing password generated.
        
