8eb9c77b66c784c51c920365a9d24ce50793d1c6
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2020-09-10 12:03:54 +0530

    Code

--- Secure password full file.py
+++ Secure password full file.py
@@ -1,29 +1,29 @@
-import string
-import random
+import string                                                     #built in module
+import random                                                     #built in module
 if __name__ == "__main__":
     print("----------What you Want to do?------------")
     print("Press 0 to generate and encrpt password")
     print("Press 1 to decrpt password")
-    e = int(input())
-    a1 = string.printable
+    e = int(input())                                              #taking input what to-do
+    a1 = string.printable                                         #taking all characters,digits and punctuations as string
     if e == 0:
 
         while True:
-	            i = input("Enter length of password: \n")
+	            i = input("Enter length of password: \n")     #checking whether input is integer.
 	            if i.isdigit():
 		            break
 	            else:
 		            print("Please enter a number")
 		            continue
-        a = []
-        a.extend(list(a1))    
-        random.shuffle(a)
+        a = []                                                   #taking empty list
+        a.extend(list(a1))                                       #appending all elements in empty list.
+        random.shuffle(a)                                        #shuffling all elements and choosing random elements
         print("Your Password is: " ,end="")
         c = "".join(a[0:int(i)])
-        print(c)
+        print(c)                                                 #printing password generated.
         while True:
-	        j = input("Set your Access Key: \n")
-	        if j.isdigit():
+	        j = input("Set your Access Key: \n")             #setting access key
+	        if j.isdigit():                                  #checking whether input is integer.
 		        break
 	        else:
 		        print("Please enter a number")
@@ -31,16 +31,16 @@ if __name__ == "__main__":
         SECURE = (('s', '$'),('i', '|'), ('x', '*'), ('k','+'), ('g','4'),('4','f'),('f','4'),('a','@'),('@','a'),('o','0'),('X','x'),('%','#'),('w','^'),('h','='),('H','~'),('r',','),('e','/'),('t','<'),('q','>'),('l','?'),('c','1'))
         def securepassword(password):
             for a,b in SECURE:
-                password = password.replace(a, b)
+                password = password.replace(a, b)                #replacing original elements to encrypted ones.
             return password
         password = c
         password = securepassword(password)
-        print(f"Your Encrpted Password is: {password}")
+        print(f"Your Encrpted Password is: {password}")          #printing encrypted password
     elif e==1:
-        f = input("Enter password to be decrpted: \n")
+        f = input("Enter password to be decrpted: \n")           #enter password to be decrypted
         while True:
-	        j = input("Enter your Access Key: \n")
-	        if j.isdigit():
+	        j = input("Enter your Access Key: \n")           #entering access key
+	        if j.isdigit():                                  #checking whether input is integer.
 		        break
 	        else:
 		        print("Please enter a number")
@@ -52,6 +52,6 @@ if __name__ == "__main__":
             return password
         password = f
         password = securepassword(password)
-        print(f"Your Password is: {password}")
-    else:
-        print("Enter 0 or 1")
+        print(f"Your Password is: {password}")                  #printing original password 
+    else:                                                       #checking condition if user input is other than 0 or 1.
+        print("Enter 0 or 1")                                   
