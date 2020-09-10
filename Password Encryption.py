948c2bb21aee0032055049da8fd8a1e00e21a0d0
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2020-09-10 11:42:12 +0530

    Code

--- Password Encryption.py
+++ Password Encryption.py
@@ -1,12 +1,10 @@
-import random
-if __name__ == "__main__":
-    inp = input("Enter password to be encrypted: \n")
-    d = int(input("Set Access Key: \n"))
-    SECURE = (('s', '$'),('i', '|'), ('x', '*'), ('k','+'), ('g','4'),('4','f'),('f','4'),('a','@'),('@','a'),('o','0'),('X','x'),('%','#'),('w','^'),('h','='),('H','~'),('r',','),('e','/'),('t','<'),('q','>'),('l','?'),('c','1'))
-    def securepassword(password):
-        for a,b in SECURE:
-            password = password.replace(a, b)
-        return password
-    password = inp
-    password = securepassword(password)
-    print(f"Your Encrpted Password is: {password}")
+inp = input("Enter password to be encrypted: \n")     #taking password to be encrypted
+d = int(input("Set Access Key: \n"))                  #setting access key
+SECURE = (('s', '$'),('i', '|'), ('x', '*'), ('k','+'), ('g','4'),('4','f'),('f','4'),('a','@'),('@','a'),('o','0'),('X','x'),('%','#'),('w','^'),('h','='),('H','~'),('r',','),('e','/'),('t','<'),('q','>'),('l','?'),('c','1'))
+def securepassword(password):
+    for a,b in SECURE:
+        password = password.replace(a, b)             #replacing orginal elements to encrypted ones.
+    return password
+password = inp
+password = securepassword(password)
+print(f"Your Encrpted Password is: {password}")       #printing encryted password
