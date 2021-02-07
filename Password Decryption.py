c1062dff9697bec752bddbb9352850d275abe959
Aayush Garg <ayushgarg1951@gmail.com>
2021-02-07 16:08:31 +0530

    Code

--- Password Decryption.py
+++ Password Decryption.py
@@ -1,7 +1,7 @@
 f = input("Enter password to be decrpted: \n")    #enter password to be dcrypted
 d = int(input("Enter Access Key: \n"))            #enter access key
         
-SECURE = (('s', '$'),('i', '|'), ('x', '*'), ('k','+'), ('g','4'),('4','f'),('f','4'),('a','@'),('@','a'),('o','0'),('X','x'),('%','#'),('w','^'),('h','='),('H','~'),('r',','),('e','/'),('t','<'),('q','>'),('l','?'),('c','1'))
+SECURE = (('s', '$'),('i', '|'), ('x', '*'), ('k','+'), ('g','4'),('a','@'),('o','0'),('X','x'),('%','#'),('w','^'),('h','='),('H','~'),('r',','),('e','/'),('t','<'),('q','>'),('l','?'),('c','1'))
 def securepassword(password):
     for a,b in SECURE:
         password = password.replace(b, a)    #replacing encrypted elements with original ones.
