b8c332fc80f2dbaa8f210b17c4682d953a3ed5fa
Aayush Garg <ayushgarg1951@gmail.com>
2021-02-07 16:07:35 +0530

    Code

--- Password Encryption.py
+++ Password Encryption.py
@@ -1,6 +1,6 @@
 inp = input("Enter password to be encrypted: \n")     #taking password to be encrypted
 d = int(input("Set Access Key: \n"))                  #setting access key
-SECURE = (('s', '$'),('i', '|'), ('x', '*'), ('k','+'), ('g','4'),('4','f'),('f','4'),('a','@'),('@','a'),('o','0'),('X','x'),('%','#'),('w','^'),('h','='),('H','~'),('r',','),('e','/'),('t','<'),('q','>'),('l','?'),('c','1'))
+SECURE = (('s', '$'),('i', '|'), ('x', '*'), ('k','+'), ('g','4'),('a','@'),('o','0'),('X','x'),('%','#'),('w','^'),('h','='),('H','~'),('r',','),('e','/'),('t','<'),('q','>'),('l','?'),('c','1'))
 def securepassword(password):
     for a,b in SECURE:
         password = password.replace(a, b)             #replacing orginal elements to encrypted ones.
